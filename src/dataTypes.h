808e0e151a624b0449e00f675874a57e94771e66
Łukasz Sowa <dantej115@gmail.com>
2023-10-22 12:13:19 +0200

    Remove data checking function, minor name changes

--- src/dataTypes.h
+++ src/dataTypes.h
@@ -4,33 +4,24 @@
 #include <ArduinoJson.h>
 #include <myRtc.h>
 
-struct dataRecived
+struct DataRecived
 {
     int16_t windDir;
     int16_t windCnt;
     int16_t inc_XVal;
     int16_t inc_YVal;
 
-    dataRecived()
+    DataRecived()
     {
         windDir = INT16_MIN;
         windCnt = INT16_MIN;
         inc_XVal = INT16_MIN;
         inc_YVal = INT16_MIN;   
     }
-    bool dataCheck()
-    {
-        if(windDir == INT16_MIN &&
-        windCnt == INT16_MIN &&
-        inc_XVal == INT16_MIN &&
-        inc_YVal == INT16_MIN )         return false;
-        // if everything good return check ok
-        return true;        
-    }
 };
 
 struct Measurements_t{
-    dataRecived _dataRecived;
+    DataRecived _dataRecived;
     dateTime_t _timeStamp;
 
     void setCurrentTime(){return _timeStamp.getDateTime();}
